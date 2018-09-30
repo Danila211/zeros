53c53fd6dad08f2883617c0cdce81e56b5751db2
danila211 <danila-211@mail.ru>
2018-09-30 23:30:37 +0300

    zerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+let count = 0;
+let i = 5;
+while(i < number){
+  count += Math.trunc(number / i);
+  i *= 5;
+  }
+  return count;
 }
+
+  
+  
+
